2da357d993b7065ef7210a125936c31f88b40e07
Edmund Yau <edmundyau@Edmunds-MBP.attlocal.net>
2024-06-22 21:35:51 -0700

    hume

---
+++ inkspire-frontend/app/page.tsx
@@ -0,0 +1,23 @@
+// ./app/page.tsx
+import ClientComponent from "@/components/ClientComponent";
+import { fetchAccessToken } from "@humeai/voice";
+
+export default async function Page() {
+  const accessToken = await fetchAccessToken({
+    apiKey: String(process.env.HUME_API_KEY),
+    secretKey: String(process.env.HUME_SECRET_KEY),
+  });
+
+  if (!accessToken) {
+    throw new Error();
+  }
+
+  return(
+  <>
+    <div>
+      <input type = "text" placeholder=" Type some text"/>
+      <ClientComponent accessToken={accessToken} />
+    </div>
+  </>
+  );
+}
